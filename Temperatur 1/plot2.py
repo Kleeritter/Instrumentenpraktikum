671a0f50b049045280829e75ba86ab1936404443
Kleeritter <93885297+Kleeritter@users.noreply.github.com>
2021-12-06 21:31:10 +0100

    goose

--- Temperatur 1/plot2.py
+++ Temperatur 1/plot2.py
@@ -2,11 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-date_time = ["2021-11-24", "2021-11-25", "2021-11-26","2021-11-27","2021-11-28","2021-11-29","2021-11-30","2021-12-01"]
+date_time = ["2021-11-17","2021-11-18","2021-11-19","2021-11-20","2021-11-21","2021-11-22","2021-11-23","2021-11-24", "2021-11-25", "2021-11-26","2021-11-27","2021-11-28","2021-11-29","2021-11-30","2021-12-01"]
 date_time = pd.to_datetime(date_time)
 
-miny=  np.loadtxt("min.txt" , usecols=(0,)) 
-maxy=  np.loadtxt("min.txt" , usecols=(1,)) 
+miny=  np.loadtxt("max.txt" , usecols=(1,)) 
+maxy=  np.loadtxt("max.txt" , usecols=(2,)) 
 
 minmessy= -10.5
 
@@ -15,13 +15,13 @@ maxmessy= 12.5
 
 
 plt.plot_date(date_time,miny,label="Min T")
-plt.plot_date(date_time,maxy,label="Max T")
+plt.plot_date(date_time,maxy,label="Max T" )
 #plt.plot_date(date_time,minmessy,label="gemessene Minimumtemperatur")
 #plt.plot_date(date_time,maxmessy,label="gemessene Maximumtemperatur")
 plt.hlines(minmessy,min(date_time), max(date_time),label="gemessene Min T")
 plt.hlines(minmessy -0.2,min(date_time), max(date_time),colors="black")
 plt.hlines(minmessy +0.2 ,min(date_time), max(date_time),colors="black")
-plt.hlines(maxmessy,min(date_time), max(date_time),label="gemessene Max T" ,colors="red")
+plt.hlines(maxmessy,min(date_time), max(date_time),label="gemessene Max T" ,colors="orange")
 plt.hlines(maxmessy -0.2,min(date_time), max(date_time),colors="black")
 plt.hlines(maxmessy +0.2 ,min(date_time), max(date_time),colors="black")
 #plt.axis([10**-9,10**-4,0.98,1.05])
@@ -29,5 +29,5 @@ plt.xlabel("Tag")
 plt.ylabel( "Temperatur in °C")
 plt.legend(bbox_to_anchor=(1,1), loc="upper left")
 #plt.tight_layout()
-plt.savefig("Tempo.png",dpi=1200)
+plt.savefig("Tempo2w.png",dpi=1200)
 plt.show()
